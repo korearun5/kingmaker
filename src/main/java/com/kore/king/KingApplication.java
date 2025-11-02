db7384a4d96d35cb729abde514b6e659014c8860
Arun Kore <korearun5@gmail.com>
2025-11-02 21:44:35 +0530

    Initial commit - base Spring Boot setup

---
+++ src/main/java/com/kore/king/KingApplication.java
@@ -0,0 +1,14 @@
+package com.kore.king;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingApplication.class, args);
+		System.out.println("Hi");
+	}
+
+}
